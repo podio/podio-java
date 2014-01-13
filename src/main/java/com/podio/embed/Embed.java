58495821dee6d6650e6c35feeb3edc69c9188a9a
daniel-sc <daniel-schreiber@gmx.de>
2014-01-13 23:26:05 +0100

    Removed custom Json deserializer and added Java Serializable support.

--- src/main/java/com/podio/embed/Embed.java
+++ src/main/java/com/podio/embed/Embed.java
@@ -1,21 +1,16 @@
 package com.podio.embed;
 
-import java.io.IOException;
+import java.io.Serializable;
 import java.util.List;
 
-import org.codehaus.jackson.JsonParser;
-import org.codehaus.jackson.JsonProcessingException;
 import org.codehaus.jackson.annotate.JsonProperty;
-import org.codehaus.jackson.map.DeserializationContext;
-import org.codehaus.jackson.map.JsonDeserializer;
-import org.codehaus.jackson.map.annotate.JsonDeserialize;
 import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
 
 import com.podio.file.File;
 
-public class Embed {
+public class Embed implements Serializable {
+
+	private static final long serialVersionUID = 1L;
 
 	/**
 	 * The id of the embed
@@ -136,7 +131,6 @@ public class Embed {
 	}
 
 	@JsonProperty("created_on")
-	@JsonDeserialize(using = DateDeserlializer.class)
 	public void setCreatedOn(DateTime createdOn) {
 		this.createdOn = createdOn;
 	}
@@ -185,18 +179,4 @@ public class Embed {
 		this.files = files;
 	}
 
-        public static class DateDeserlializer extends JsonDeserializer<DateTime> {
-                
-                public DateDeserlializer() {
-                    super();
-                }
-            
-                @Override
-                public DateTime deserialize(JsonParser arg0, DeserializationContext arg1) throws IOException,
-                	JsonProcessingException {
-                    DateTimeFormatter formatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm:ss");
-                    return formatter.parseDateTime(arg0.getText());
-                }
-    	    
-            }
 }
