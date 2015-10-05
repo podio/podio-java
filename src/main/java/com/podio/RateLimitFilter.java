612df4094c76878794dacbedc0f3d57c72620925
PetrFOX <petaliska@gmail.com>
2015-10-05 22:04:00 +0200

    case without set headers fixed, comments added, formatting changed

--- src/main/java/com/podio/RateLimitFilter.java
+++ src/main/java/com/podio/RateLimitFilter.java
@@ -3,15 +3,41 @@ package com.podio;
 import com.sun.jersey.api.client.ClientRequest;
 import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.filter.ClientFilter;
+import java.util.List;
+import javax.ws.rs.core.MultivaluedMap;
 
 public class RateLimitFilter extends ClientFilter {
 
-	@SuppressWarnings("unchecked")
-	@Override
-	public ClientResponse handle(ClientRequest cr) {
-                ClientResponse response = getNext().handle(cr);
-                RateLimits.setLimit(Integer.parseInt(response.getHeaders().get("X-Rate-Limit-Limit").get(0)));
-                RateLimits.setRemaining(Integer.parseInt(response.getHeaders().get("X-Rate-Limit-Remaining").get(0)));
-                return response;
-	}
+    @SuppressWarnings("unchecked")
+    @Override
+    public ClientResponse handle(ClientRequest cr) {
+            ClientResponse response = getNext().handle(cr);
+            MultivaluedMap<String, String> headers = response.getHeaders();
+            if (headers != null) {
+                    List<String> limitHeader = headers.get("X-Rate-Limit-Limit");
+                    if (limitHeader != null && limitHeader.size() == 1) {
+                            try {
+                                    RateLimits.setLimit(Integer.parseInt(limitHeader.get(0)));
+                            } catch (NumberFormatException nfe) {
+                                    RateLimits.setLimit(null);
+                            }
+                    } else {
+                            RateLimits.setLimit(null);
+                    }
+                    List<String> remainingHeader = response.getHeaders().get("X-Rate-Limit-Remaining");
+                    if (remainingHeader != null && remainingHeader.size() == 1) {
+                            try {
+                                    RateLimits.setRemaining(Integer.parseInt(remainingHeader.get(0)));
+                            } catch (NumberFormatException nfe) {
+                                    RateLimits.setRemaining(null);
+                            }
+                    } else {
+                            RateLimits.setRemaining(null);
+                    }
+            } else {
+                    RateLimits.setLimit(null);
+                    RateLimits.setRemaining(null);
+            }
+            return response;
+    }
 }
--- src/main/java/com/podio/RateLimits.java
+++ src/main/java/com/podio/RateLimits.java
@@ -1,24 +1,37 @@
 package com.podio;
 
 public class RateLimits {
-    
-        private static int limit;
-        private static int remaining;
 
-        public static int getLimit() {
+        private static Integer limit;
+        private static Integer remaining;
+
+        /**
+         * Returns the ceiling for the request you just made. May be null if
+         * there is no limit.
+         *
+         * @return the ceiling for the request you just made
+         */
+        public static Integer getLimit() {
                 return limit;
         }
 
-        public static void setLimit(int limit) {
+        public static void setLimit(Integer limit) {
                 RateLimits.limit = limit;
         }
 
-        public static int getRemaining() {
+        /**
+         * Returns the number of requests you have left for the current 1 hour
+         * window. May be null if there is no limit.
+         *
+         * @return the number of requests you have left for the current 1 hour
+         * window
+         */
+        public static Integer getRemaining() {
                 return remaining;
         }
 
-        public static void setRemaining(int remaining) {
+        public static void setRemaining(Integer remaining) {
                 RateLimits.remaining = remaining;
         }
-    
+
 }
