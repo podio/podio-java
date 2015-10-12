f21898debb6eb1e5d80e3ced8288c452dccd5c29
PetrFOX <petaliska@gmail.com>
2015-10-12 22:01:34 +0200

    added missing parameters in getItemsByFieldAndTitle method

--- src/main/java/com/podio/item/ItemAPI.java
+++ src/main/java/com/podio/item/ItemAPI.java
@@ -7,6 +7,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.podio.BaseAPI;
 import com.podio.ResourceFactory;
+import com.podio.common.ToStringUtil;
 import com.podio.filter.ExternalIdFilterBy;
 import com.podio.filter.FilterByValue;
 import com.podio.filter.SortBy;
@@ -176,13 +177,25 @@ public class ItemAPI extends BaseAPI {
 	 *            The id of app reference field to search for
 	 * @param text
 	 *            The text to search for in the items title
+         * @param notItemIds
+         *            If supplied the items with these ids will not be returned
+         * @param limit
+         *            The maximum number of results to return. Default value: 13
 	 * @return The items that were valid for the field and with text matching
 	 */
-	public List<ItemMini> getItemsByFieldAndTitle(int fieldId, String text) {
-		return getResourceFactory()
-				.getApiResource("/item/field/" + fieldId + "/find")
-				.queryParam("text", text)
-				.get(new GenericType<List<ItemMini>>() {
+	public List<ItemMini> getItemsByFieldAndTitle(int fieldId, String text,
+                        List<Integer> notItemIds, Integer limit) {
+                WebResource resource = getResourceFactory().getApiResource(
+                                "/item/field/" + fieldId + "/find");
+                if (limit != null) {
+			resource = resource.queryParam("limit", limit.toString());
+		}
+                if (notItemIds != null && notItemIds.size() > 0) {
+			resource = resource.queryParam(
+                                        "not_item_id", ToStringUtil.toString(notItemIds, ","));
+		}
+                resource = resource.queryParam("text", text);
+		return resource.get(new GenericType<List<ItemMini>>() {
 				});
 	}
 
