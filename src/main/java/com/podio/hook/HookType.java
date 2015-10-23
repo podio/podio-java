e89aeea5a7383d9ed91d595c0f790d5ff9e9cf6d
Albert Fernández <albertfdp@gmail.com>
2015-10-23 14:37:35 +0200

    fix indentation

--- src/main/java/com/podio/hook/HookType.java
+++ src/main/java/com/podio/hook/HookType.java
@@ -6,13 +6,13 @@ import org.codehaus.jackson.annotate.JsonValue;
 public enum HookType {
 
 	ITEM_CREATE("item.create"),
-        ITEM_UPDATE("item.update"),
-        ITEM_DELETE("item.delete"),
-        COMMENT_CREATE("comment.create"),
-        COMMENT_DELETE("comment.delete"),
-        FILE_CHANGE("file.change"),
-        APP_UPDATE("app.update"),
-        APP_DELETE("app.delete");
+	ITEM_UPDATE("item.update"),
+	ITEM_DELETE("item.delete"),
+	COMMENT_CREATE("comment.create"),
+	COMMENT_DELETE("comment.delete"),
+	FILE_CHANGE("file.change"),
+	APP_UPDATE("app.update"),
+	APP_DELETE("app.delete");
 
 	private final String externalName;
 
